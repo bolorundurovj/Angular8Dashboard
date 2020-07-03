0d793544b8352739c26e0918ea6f8129b794ce1e
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-03 13:38:57 +0100

    Basic Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DefaultComponent} from './layouts/default/default.component';
+import {DashboardComponent} from './modules/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+    component: DefaultComponent,
+    children: [
+      { path: '', component: DashboardComponent }
+      ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
