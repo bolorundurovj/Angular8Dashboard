f02e6878652df5fdd83ce72d4f099b875db5b1ff
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-04 12:09:57 +0100

    Setup Data Cards

---
+++ src/app/shared/widgets/card/card.component.ts
@@ -0,0 +1,77 @@
+import {Component, Input, OnInit} from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+// HC_exporting(Highcharts);
+
+@Component({
+  selector: 'app-widget-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  Highcharts = Highcharts;
+  chartOptions: {};
+  @Input() label: string;
+  @Input() percentage: string;
+  @Input() total: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+        backgroundColor: null,
+        borderWidth: 0,
+        margin: [2, 2, 2, 2],
+        height: 100
+      },
+      title: {
+        text: null
+      },
+      subtitle: {
+        text: null
+      },
+      tooltip: {
+        split: true,
+        outside: true
+      },
+      xAxis: {
+        labels: {
+          enabled: false
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      yAxis: {
+        labels: {
+          enabled: false
+        },
+        title: {
+          text: null
+        },
+        startOnTick: false,
+        endOnTick: false,
+        tickOptions: []
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false
+      },
+      series: [{
+        data: [53, 60, 45, 55, 50]
+      }]
+    };
+    HC_exporting(Highcharts);
+  }
+
+}
