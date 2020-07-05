7aba620905b1b7fd4ed3e6afaaf2a883fe69f88a
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-05 20:01:21 +0100

    Changed Data Source

--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import * as Highcharts from 'highcharts';
 import HC_exporting from 'highcharts/modules/exporting';
 // HC_exporting(Highcharts);
@@ -11,6 +11,8 @@ import HC_exporting from 'highcharts/modules/exporting';
 export class AreaComponent implements OnInit {
   Highcharts = Highcharts;
   chartOptions: {};
+
+  @Input() data = [];
 
   constructor() { }
 
@@ -64,22 +66,7 @@ export class AreaComponent implements OnInit {
       exporting: {
         enabled: true
       },
-      series: [{
-        name: 'Asia',
-        data: [502, 635, 809, 947, 1402, 3634, 5268]
-      }, {
-        name: 'Africa',
-        data: [106, 107, 111, 133, 221, 767, 1766]
-      }, {
-        name: 'Europe',
-        data: [163, 203, 276, 408, 547, 729, 628]
-      }, {
-        name: 'America',
-        data: [18, 31, 54, 156, 339, 818, 1201]
-      }, {
-        name: 'Oceania',
-        data: [2, 2, 2, 6, 13, 30, 46]
-      }]
+      series: this.data
     };
     HC_exporting(Highcharts);
     setTimeout(() => {
