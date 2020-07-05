a8f70e241a326b6d23392f800c4bc8dc002c6a9f
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-05 20:00:48 +0100

    Setup PieChart Widget

---
+++ src/app/shared/widgets/pie/pie.component.ts
@@ -0,0 +1,61 @@
+import {Component, Input, OnInit} from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-widget-pie',
+  templateUrl: './pie.component.html',
+  styleUrls: ['./pie.component.scss']
+})
+export class PieComponent implements OnInit {
+  Highcharts = Highcharts;
+  chartOptions: {};
+  @Input() data = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+      },
+      title: {
+        text: 'Random Data'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      accessibility: {
+        point: {
+          valueSuffix: '%'
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      exporting: {
+        enabled: true
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+        }
+      },
+      series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: this.data
+      }]
+    };
+    HC_exporting(Highcharts);
+  }
+
+}
